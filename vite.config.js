31396b70e3fb1d7638e2932746c018f3f35cba1f
isYoshihiro <is.Yoshihiro@gmail.com>
2025-12-08 21:59:18 +0900

    set docs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-	build: {
-		outDir: 'docs',
-	},
+  base: '/excel-lambda-optimizer/',
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
